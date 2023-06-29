bac5c45c67f04dbea9f8942f64bd68057e162602
Vlad Ogay <ogay.vlad@gmail.com>
2023-06-29 05:51:49 +0400

    Styles for PDF viewer

--- src/Viewer.tsx
+++ src/Viewer.tsx
@@ -3,12 +3,50 @@ import * as pdfjs from "pdfjs-dist/webpack"
 import { PDFViewer, PDFFindController, PDFLinkService, EventBus } from "pdfjs-dist/web/pdf_viewer.js"
 import { styled } from "@mui/material"
 
-const Container = styled("div")({
+const ContainerDiv = styled("div")({
     position: "absolute",
     border: "1px solid white",
-    left: "300px",
-    width: "350px",
-    height: "200px",
+    left: "10%",
+    width: "70%",
+    height: "500px",
+    overflow: "auto",
+})
+const ViewerDiv = styled("div")({
+    position: "relative",
+
+    ".page": {
+        direction: "ltr",
+        position: "relative",
+        overflow: "visible",
+    },
+
+    ".textLayer": {
+        position: "absolute",
+        left: 0,
+        top: 0,
+        right: 0,
+        bottom: 0,
+        lineHeight: 1,
+        opacity: 1
+    },
+    ".textLayer span": {
+        color: "black",
+        position: "absolute",
+        whiteSpace: "pre",
+        transformOrigin: "0% 0%",
+        border: "solid 1px rgba(255, 0, 0, 0.5)",
+        backgroundColor: "rgba(255, 255, 32, 0.1)",
+        boxSizing: "border-box"
+    },
+    ".textLayer br": {
+        color: "black",
+        position: "absolute",
+        whiteSpace: "pre",
+        transformOrigin: "0% 0%",
+        border: "solid 1px rgba(255, 0, 0, 0.5)",
+        backgroundColor: "rgba(255, 255, 32, 0.1)",
+        boxSizing: "border-box"
+    }
 })
 
 export const Viewer = ({ src }: {src: string}) => {
@@ -47,9 +85,9 @@ export const Viewer = ({ src }: {src: string}) => {
     }, [])
 
     return (
-        <Container ref={containerRef}>
-            <div id="viewer" className="pdfViewer" />
-        </Container>
+        <ContainerDiv ref={containerRef}>
+            <ViewerDiv />
+        </ContainerDiv>
     )
 }
 
